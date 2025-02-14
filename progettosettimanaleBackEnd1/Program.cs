7ba90db3b38d7e891b0aa78f21ab2cdf1e93dd6f
rachelebarberis <rachele.barberis.12@gmail.com>
2025-02-14 12:25:00 +0100

    controllo sulla data corretta

--- progettosettimanaleBackEnd1/Program.cs
+++ progettosettimanaleBackEnd1/Program.cs
@@ -8,8 +8,15 @@ contribuente.Nome = Console.ReadLine();
 Console.WriteLine("Inserisci il tuo cognome: ");
 contribuente.Cognome = Console.ReadLine();
 
-Console.WriteLine("Inserisci la tua data di nasciata (dd/MM/yyyy): ");
-contribuente.DataNascita = DateTime.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci la tua data di nascita (dd/MM/yyyy): ");
+DateTime dataNascita;
+
+while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dataNascita))
+{
+    Console.WriteLine("Formato data non valido. Per favore inserisci la data nel formato dd/MM/yyyy: ");
+}
+
+contribuente.DataNascita = dataNascita;
 
 Console.WriteLine("Inserisci il tuo codice fiscale: ");
 contribuente.CodiceFiscale = Console.ReadLine();
